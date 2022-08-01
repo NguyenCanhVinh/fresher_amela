3a517a893da23ac98a45c390af4c0bc71c9e0091
Demo <67863427+NguyenCanhVinh@users.noreply.github.com>
2022-08-01 11:45:07 +0700

    update delete

--- TaskManager_web/src/main/java/vn/amela/fresher/controller/TaskController.java
+++ TaskManager_web/src/main/java/vn/amela/fresher/controller/TaskController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -50,16 +51,15 @@ public class TaskController {
                              @RequestParam(value = "title", required = false) String title, @RequestParam("size") Optional<Integer> size,
                               @RequestParam("brand") Optional<Long> brandPage) {
 
-
         int currentPage = page.orElse(0);
         int pageSize = size.orElse(5);
+
         if (title.equalsIgnoreCase("null")) {
             title = "";
         }
         Long brand = brandPage.orElse(0L);
 
         Pageable pageable = PageRequest.of(currentPage, pageSize);
-
 
 
         Page<Task> list = null;
@@ -73,10 +73,42 @@ public class TaskController {
         model.addAttribute("brand", brand);
         model.addAttribute("tasks", list);
         model.addAttribute("title", title);
+
         List<TaskStatus> listC = taskStatusRepository.findAll();
         model.addAttribute("taskStatus", listC);
-        // set active front-end
-        model.addAttribute("menuP", "menu");
+
         return new ModelAndView("admin/list", model);
+    }
+
+    @RequestMapping("/search")
+    public ModelAndView search(ModelMap model, @RequestParam("title") String title,
+                               @RequestParam("size") Optional<Integer> size) {
+        int pageSize = size.orElse(5);
+        Pageable pageable = PageRequest.of(0, pageSize);
+
+        Page<Task> list = taskRepository.findByTitleContaining(title, pageable);
+
+        model.addAttribute("title", title);
+        model.addAttribute("tasks", list);
+
+        List<TaskStatus> listC = taskStatusRepository.findAll();
+        model.addAttribute("taskStatus", listC);
+
+        return new ModelAndView("admin/list", model);
+    }
+
+    @GetMapping("/delete/{task_id}")
+    public ModelAndView delete(@PathVariable("task_id") Long task_id, ModelMap model) {
+
+        Optional<Task> opt=taskRepository.findById(task_id);
+        if (opt.isPresent()){
+
+            taskRepository.delete(opt.get());
+            model.addAttribute("message", "Xoá thành công!");
+        }
+        else {
+            model.addAttribute("error", "Không thể task này!");
+        }
+        return new ModelAndView("forward:/task", model);
     }
 }
